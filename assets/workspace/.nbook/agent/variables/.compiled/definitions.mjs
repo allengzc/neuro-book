e1016c62feb497ef61f14e41b844fada7a3d5be3
notnotype <notnotype@gmail.com>
2026-06-05 19:52:41 +0800

    build: prepare system assets for product release

--- assets/workspace/.nbook/agent/variables/.compiled/definitions.mjs
+++
@@ -1,7 +0,0 @@
-// assets/workspace/.nbook/agent/variables/definitions.ts
-var definitions = [];
-var definitions_default = definitions;
-export {
-  definitions_default as default,
-  definitions
-};
